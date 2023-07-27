d39b474012b16e321433afa5ee26294380210cd7
Abdulaziz <aa.alnuaimi99@icloud.com>
2023-07-27 09:44:17 +0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Abdulaziz AL Nuaimi on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
